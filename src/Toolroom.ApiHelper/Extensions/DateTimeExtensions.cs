ab1da98e4626fe82297707e2664e4816b46c9834
Peter Malli-Trinkl <p.malli@toolroom.at>
2018-02-20 12:34:04 +0100

    Additional UtcDateTimeExtensions

--- src/Toolroom.ApiHelper/Extensions/DateTimeExtensions.cs
+++ src/Toolroom.ApiHelper/Extensions/DateTimeExtensions.cs
@@ -11,5 +11,36 @@ namespace Toolroom.ApiHelper
             tz = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
             return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, tz);
         }
+        public static DateTime? FromUtcToLocalDateTime(this DateTime? utcDateTime, string timeZoneId = DefaultTimeZoneId)
+        {
+            if (!utcDateTime.HasValue)
+                return null;
+            TimeZoneInfo tz;
+            tz = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+            return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime.Value, tz);
+        }
+
+        public static DateTime FromLocalToUtcDateTime(this DateTime localDateTime, string timeZoneId = DefaultTimeZoneId)
+        {
+            TimeZoneInfo tz;
+            tz = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+            return TimeZoneInfo.ConvertTime(localDateTime, tz, TimeZoneInfo.Utc);
+        }
+
+        public static DateTime? FromLocalToUtcDateTime(this DateTime? localDateTime, string timeZoneId = DefaultTimeZoneId)
+        {
+            if (!localDateTime.HasValue)
+            {
+                return null;
+            }
+            TimeZoneInfo tz;
+            tz = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+            return TimeZoneInfo.ConvertTime(localDateTime.Value, tz, TimeZoneInfo.Utc);
+        }
+
+        public static DateTime ConvertToUnspecifiedDateTime(this DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, DateTimeKind.Unspecified);
+        }
     }
 }
