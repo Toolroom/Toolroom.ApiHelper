f7ed3c4f81af6d9b3718e8692562f70714a2d6e4
Peter Malli-Trinkl <p.malli@toolroom.at>
2018-03-02 13:15:43 +0100

    added DateTime rounding methods

--- src/Toolroom.ApiHelper/Extensions/DateTimeExtensions.cs
+++ src/Toolroom.ApiHelper/Extensions/DateTimeExtensions.cs
@@ -5,13 +5,16 @@ namespace Toolroom.ApiHelper
     public static class DateTimeExtensions
     {
         private const string DefaultTimeZoneId = "W. Europe Standard Time";
+
         public static DateTime FromUtcToLocalDateTime(this DateTime utcDateTime, string timeZoneId = DefaultTimeZoneId)
         {
             TimeZoneInfo tz;
             tz = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
             return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, tz);
         }
-        public static DateTime? FromUtcToLocalDateTime(this DateTime? utcDateTime, string timeZoneId = DefaultTimeZoneId)
+
+        public static DateTime? FromUtcToLocalDateTime(this DateTime? utcDateTime,
+            string timeZoneId = DefaultTimeZoneId)
         {
             if (!utcDateTime.HasValue)
                 return null;
@@ -20,14 +23,16 @@ namespace Toolroom.ApiHelper
             return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime.Value, tz);
         }
 
-        public static DateTime FromLocalToUtcDateTime(this DateTime localDateTime, string timeZoneId = DefaultTimeZoneId)
+        public static DateTime FromLocalToUtcDateTime(this DateTime localDateTime,
+            string timeZoneId = DefaultTimeZoneId)
         {
             TimeZoneInfo tz;
             tz = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
             return TimeZoneInfo.ConvertTime(localDateTime, tz, TimeZoneInfo.Utc);
         }
 
-        public static DateTime? FromLocalToUtcDateTime(this DateTime? localDateTime, string timeZoneId = DefaultTimeZoneId)
+        public static DateTime? FromLocalToUtcDateTime(this DateTime? localDateTime,
+            string timeZoneId = DefaultTimeZoneId)
         {
             if (!localDateTime.HasValue)
             {
@@ -41,6 +46,25 @@ namespace Toolroom.ApiHelper
         public static DateTime ConvertToUnspecifiedDateTime(this DateTime dt)
         {
             return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, DateTimeKind.Unspecified);
+        }
+
+
+        public static DateTime Floor(this DateTime dt, TimeSpan ts)
+        {
+            long ticks = dt.Ticks / ts.Ticks;
+            return new DateTime(ticks * ts.Ticks);
+        }
+
+        public static DateTime Round(this DateTime dt, TimeSpan ts)
+        {
+            long ticks = (dt.Ticks + (ts.Ticks / 2) + 1) / ts.Ticks;
+            return new DateTime(ticks * ts.Ticks);
+        }
+
+        public static DateTime Ceiling(this DateTime dt, TimeSpan ts)
+        {
+            long ticks = (dt.Ticks + ts.Ticks - 1) / ts.Ticks;
+            return new DateTime(ticks * ts.Ticks);
         }
     }
 }
