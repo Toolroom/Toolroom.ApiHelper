f74f9741640f7faab46d87f2527f7d4df6ee5a0b
Peter Malli-Trinkl <p.malli@toolroom.at>
2018-10-09 22:37:22 +0200

    cache supporting get or add for multiple items

--- src/Toolroom.ApiHelper/Cache/RedisCache.cs
+++ src/Toolroom.ApiHelper/Cache/RedisCache.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using StackExchange.Redis;
 
@@ -34,7 +36,7 @@ namespace Toolroom.ApiHelper
             var server = Connection.GetServer(_connectionString.Split(',')[0]);
             if (!server.IsConnected) return false;
 
-            var keys = server.Keys(Db.Database, pattern: GetKey(typeof(T), "*")).ToArray();
+            var keys = server.Keys(Db.Database, pattern: GetKey(typeof(T), "*").ToString()).ToArray();
             if (!IsConnected) return false;
             var deletedItems = Db.KeyDelete(keys);
             return true;
@@ -47,26 +49,50 @@ namespace Toolroom.ApiHelper
             return !Db.KeyExists(key) || Db.KeyDelete(key);
         }
 
-        private string GetKey<TKey>(Type type, TKey id)
+        private IEnumerable<KeyValuePair<TKey, RedisKey>> GetKeys<TKey>(Type type, IEnumerable<TKey> ids)
+        {
+            return GetKeys(type.FullName, ids);
+        }
+
+        private RedisKey GetKey<TKey>(Type type, TKey id)
         {
             return GetKey(type.FullName, id);
         }
-        private string GetKey<TKey>(string typeName, TKey id)
+        private RedisKey GetKey<TKey>(string typeName, TKey id)
         {
             return $"{typeName}:{id}";
         }
-
-        public bool Set<TKey, T>(TKey id, T item)
+        private IEnumerable<KeyValuePair<TKey, RedisKey>> GetKeys<TKey>(string typeName, IEnumerable<TKey> ids)
         {
-            var key = GetKey(typeof(T), item);
+            foreach (var id in ids)
+            {
+                yield return new KeyValuePair<TKey, RedisKey>(id, $"{typeName}:{id}");
+            }
+        }
+
+        private bool Set<TKey, T>(TKey id, T item)
+        {
+            var key = GetKey(typeof(T), id);
             return Set(key, item);
         }
 
-        private bool Set<T>(string key, T item)
+        private bool Set<T>(RedisKey key, T item)
         {
             if (!IsConnected) return false;
             var serializedItem = SerializeObject(item);
             return Db.StringSet(key, serializedItem);
+        }
+
+        private bool SetMany<TKey, T>(IEnumerable<T> items, Func<T, TKey> idSelector)
+        {
+            if (!IsConnected) return false;
+            var list = new List<KeyValuePair<RedisKey, RedisValue>>();
+            var typeName = typeof(T).FullName;
+            foreach (var item in items)
+            {
+                list.Add(new KeyValuePair<RedisKey, RedisValue>(GetKey(typeName, idSelector(item)), SerializeObject(item)));
+            }
+            return Db.StringSet(list.ToArray());
         }
 
         public void AddOrUpdate<TKey, T>(TKey id, T item)
@@ -110,6 +136,67 @@ namespace Toolroom.ApiHelper
             var newVal = valueFactory(id);
             Set(id, newVal);
             return newVal;
+        }
+
+        public async Task<ICollection<T>> GetOrAdd<TKey, T>(IEnumerable<TKey> ids, Func<IEnumerable<TKey>, Task<ICollection<T>>> valueFactory, Func<T, TKey> idSelector)
+        {
+            var ret = new List<T>();
+            if (ids == null)
+                throw new ArgumentNullException(nameof(ids));
+
+            if (valueFactory == null)
+                throw new ArgumentNullException(nameof(valueFactory));
+
+            if (idSelector == null)
+                throw new ArgumentNullException(nameof(idSelector));
+
+            if (!IsConnected)
+            {
+                foreach (var retValue in await valueFactory(ids))
+                {
+                    ret.Add(retValue);
+                }
+                return ret;
+            }
+
+            var keys = GetKeys(typeof(T), ids).ToDictionary(_ => _.Key, _ => _.Value);
+            var missingIds = new List<TKey>();
+            var storedVals = Db.StringGet(keys.Values.ToArray());
+            for (int i = 0; i < storedVals.Length; i++)
+            {
+                var storedVal = storedVals[i];
+                if (storedVal.HasValue)
+                {
+                    var element = default(T);
+                    try
+                    {
+                        element = DeserializeObject<T>(storedVal);
+                    }
+                    catch
+                    {
+                        // ignored - cannot deserialize - must be refreshed
+                        missingIds.Add(keys.ElementAt(i).Key);
+                        break;
+                    }
+                    if (element != null && !element.Equals(default(T)))
+                        ret.Add(element);
+                    else
+                        missingIds.Add(keys.ElementAt(i).Key);
+                }
+                else
+                    missingIds.Add(keys.ElementAt(i).Key);
+            }
+            if (missingIds.Any())
+            {
+                var list = new List<T>();
+                foreach (var missingElement in await valueFactory(missingIds))
+                {
+                    list.Add(missingElement);
+                    ret.Add(missingElement);
+                }
+                SetMany(list, idSelector);
+            }
+            return ret;
         }
 
         private string SerializeObject(object objectToCache)
